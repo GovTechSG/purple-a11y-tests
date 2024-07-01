6f7a18a801d11be09b296f66c828e5d0992acfb4
Joshua Lai <joshualai9922@gmail.com>
2024-07-01 10:16:41 +0800

    feat: happy flow following integration.md example

---
+++ cypress.config.ts
@@ -0,0 +1,64 @@
+import { defineConfig } from "cypress";
+import purpleA11yInit from "purplea11y";
+
+interface ViewportSettings {
+    width: number;
+    height: number;
+}
+
+interface Thresholds {
+    mustFix: number;
+    goodToFix: number;
+}
+
+interface ScanAboutMetadata {
+    browser: string;
+}
+
+// viewport used in tests to optimise screenshots
+const viewportSettings: ViewportSettings = { width: 1920, height: 1040 };
+// specifies the number of occurrences before error is thrown for test failure
+const thresholds: Thresholds = { mustFix: 20, goodToFix: 20 };
+// additional information to include in the "Scan About" section of the report
+const scanAboutMetadata: ScanAboutMetadata = { browser: 'Chrome (Desktop)' };
+
+const purpleA11y = await purpleA11yInit(
+    "https://govtechsg.github.io", // initial url to start scan
+    "purplea11y test", // label for test
+    "PurpleA11y functional test",
+    "accessibility@tech.gov.sg",
+    true, // include screenshots of affected elements in the report
+    viewportSettings,
+    thresholds,
+    scanAboutMetadata,
+);
+
+export default defineConfig({
+    taskTimeout: 120000, // need to extend as screenshot function requires some time
+    viewportHeight: viewportSettings.height,
+    viewportWidth: viewportSettings.width,
+    e2e: {
+        setupNodeEvents(on, _config) {
+            on("task", {
+                getPurpleA11yScripts(): string {
+                    return purpleA11y.getScripts();
+                },
+                async pushPurpleA11yScanResults({res, metadata, elementsToClick}: { res: any, metadata: any, elementsToClick: any[] }): Promise<{ mustFix: number, goodToFix: number }> {
+                    return await purpleA11y.pushScanResults(res, metadata, elementsToClick);
+                },
+                returnResultsDir(): string {
+                    return `results/${purpleA11y.randomToken}_${purpleA11y.scanDetails.urlsCrawled.scanned.length}pages/reports/report.html`;
+                },
+                finishPurpleA11yTestCase(): null {
+                    purpleA11y.testThresholds();
+                    return null;
+                },
+                async terminatePurpleA11y(): Promise<null> {
+                    return await purpleA11y.terminate();
+                },
+            });
+        },
+        supportFile: 'dist/cypress/support/e2e.js',
+        specPattern: 'dist/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    },
+});
