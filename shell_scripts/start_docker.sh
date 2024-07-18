73f95950f8e5c8f6225d1f71c610c7b35fd0913c
LeeYiXuan21 <S10243281@connect.np.edu.sg>
2024-07-18 13:34:22 +0800

    check for container first

--- shell_scripts/start_docker.sh
+++ shell_scripts/start_docker.sh
@@ -1,11 +1,13 @@
-# start colima
-colima start
+# Check if the container is running and remove it if necessary
+if [ "$(docker ps -a | grep purple-a11y-instances)" ]; then
+  docker rm -f purple-a11y-instances
+fi
 
-#build docker container
+# Build the Docker container
 docker build -t purple-a11y-tests .
 
-# Add hostname mappings and run container
+# Add hostname mappings and run the container
 docker run -it --name purple-a11y-instances --add-host=main.purple.com:0.0.0.0 --add-host=diffHostname.purple.com:0.0.0.0 purple-a11y-tests
 
-# Docker container to edit
+# Access the container's bash shell
 docker exec -it purple-a11y-instances /bin/bash
