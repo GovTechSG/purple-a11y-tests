5e53b5d00b24cb2bd3d39ea3530499546a270c8f
CKodes <ckodes@outlook.com>
2024-07-22 18:18:35 +0800

    refactor: remove p flag for env vars and generate command via getCliCommand

--- src/cypress/e2e/custom.cy.ts
+++ src/cypress/e2e/custom.cy.ts
@@ -1,15 +1,33 @@
-describe('Run Custom Flow CLI Command', () => {
-  it('should trigger a custom flow > scan one page > close the browser > save results', () => {
-    cy.exec(
-      'cd node_modules/@govtechsg/purple-hats && npm run cli -- -c 3 -k kc:accessibility@tech.gov.sg -u https://www.tech.gov.sg -h no',
-      {
-        env: { IS_CYPRESS_TEST: 'true' },
-      }
-    ).then((result) => {
-      expect(result.code).to.equal(0)
+import { getCliCommand } from '../support/e2e.js'
 
-      cy.log(result.stdout)
-      cy.log(result.stderr)
+// -c 3
+const constantCliOptionsJson = { c: Cypress.env('customFlowCliOption') }
+
+// remove p flag
+const { p, ...cliOptionsCustomFlowWithoutP } = Cypress.env(
+  'cliOptionsCustomFlow'
+)
+
+context('CustomFlow', () => {
+  const cliOptionsCustomFlow = {
+    ...constantCliOptionsJson,
+    ...cliOptionsCustomFlowWithoutP,
+  }
+
+  const getCustomFlowCliCommand = getCliCommand(cliOptionsCustomFlow, true)
+
+  describe('Run Custom Flow CLI Command', () => {
+    it('should trigger a custom flow > scan one page > close the browser > save results', () => {
+      cy.exec(
+        `cd ${Cypress.env('purpleA11yPath')} && ${getCustomFlowCliCommand}`,
+        {
+          env: { IS_CYPRESS_TEST: 'true' },
+        }
+      ).then((result) => {
+        expect(result.code).to.equal(0)
+        cy.log(result.stdout)
+        cy.log(result.stderr)
+      })
     })
   })
 })
